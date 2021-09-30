9911d3cddab2eb4e49fdbb442f4bd553e1c4c553
Nikita-Tihomirov <nikita_tikhomirov@epam.com>
2021-09-30 14:48:55 +0300

    Added tested method, workload generator app, controller

---
+++ TestAsyncHelperApp/Program.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading;
+using System.Threading.Tasks;
+using TestWebApp;
+
+namespace TestAsyncHelperApp
+{
+	class Program
+	{
+		static void Main(string[] args)
+        {
+            //ThreadPool.SetMaxThreads(1000,1000);
+
+            var hostBuilder = new WebHostBuilder()
+			.UseKestrel()
+			.UseStartup<Startup>()
+			.Build();
+
+			hostBuilder.RunAsync();
+
+			HttpClientHandler clientHandler = new HttpClientHandler();
+			clientHandler.ServerCertificateCustomValidationCallback = (sender, cert, chain, sslPolicyErrors) => { return true; };
+			HttpClient client = new HttpClient(clientHandler);
+
+			MediaTypeHeaderValue mediaTypeHeaderValue = new MediaTypeHeaderValue("application/json");
+            var tasks = new List<Task>();
+            for (int i = 0; i < 700; i++)
+            {
+                int x = i;
+                var t = Task.Run(async () =>
+                {
+                    var content = new StringContent("\"" + Guid.NewGuid().ToString() + "\"");
+                    content.Headers.ContentType = mediaTypeHeaderValue;
+                    await client.PostAsync("http://localhost:5000/api/values", content);
+                    Console.WriteLine(x);
+                });
+                tasks.Add(t);
+            }
+            Task.WhenAll(tasks).Wait();
+        }
+	}
+}
---
+++ TestWebApp/Controllers/ValuesController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace TestWebApp.Controllers
+{
+	public class ValuesController : ApiController
+    {
+        public string Post([FromBody]string value)
+        {
+            return AsyncHelper.RunSync(PostAsync);
+        }
+
+        private async Task<string> PostAsync()
+        {
+            await Task.Delay(10000);
+            return "Hello World";
+        }
+    }
+}
